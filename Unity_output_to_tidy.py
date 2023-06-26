b437b2fa1f861044a2debecdc680e8db12c00fde
Benjamín <dupre.benjamin@gmail.com>
2023-06-26 12:07:04 +0000

    added path and list not yet.

--- Unity_output_to_tidy.py
+++ Unity_output_to_tidy.py
@@ -2,17 +2,17 @@
 import dropbox
 
 # Set up the Dropbox API client
-access_token = 'sl.BgsW-iMmoGvliSYK-WjPhheeYNPKwjxgiNmdMuQA6G1HUuysD3YSStMG4qJDMsdGMLi1G9VjI1c4NC8BYiZ8FInQ5yaIRKnt_egzuC87vUE0r9k8-IT6QPokkK3DaeviGFvNvbH1hqVO'
-dbx = dropbox.Dropbox(access_token)
+ACCES_TOKEN = 'sl.BgsW-iMmoGvliSYK-WjPhheeYNPKwjxgiNmdMuQA6G1HUuysD3YSStMG4qJDMsdGMLi1G9VjI1c4NC8BYiZ8FInQ5yaIRKnt_egzuC87vUE0r9k8-IT6QPokkK3DaeviGFvNvbH1hqVO'
+dbx = dropbox.Dropbox(ACCES_TOKEN)
 
 # Specify the path to the file you want to access
 #folder_path = ""#'/backups/My Mac (glaroam2-185-117.wireless.gla.ac.uk)/Documents/Research MaxPlank/P1_propioception/Data_Wrangling/Matlab Analysis/Data_Wrangling'
 
-#'/backups/My Mac (glaroam2-185-117.wireless.gla.ac.uk)/Documents/Research MaxPlank/P1_propioception/Data_Wrangling/Matlab Analysis/Data_Wrangling'
+FOLDER_PATH='/My Mac (glaroam2-185-117.wireless.gla.ac.uk)/Documents/Research MaxPlank/P1_propioception/Data_Wrangling/Matlab Analysis/Data_Wrangling'
 #response = dbx.files_list_folder(folder_path)
 
 try:
-    response = dbx.files_list_folder('')
+    response = dbx.files_list_folder(FOLDER_PATH)
     # Iterate over the entries in the root directory
     for entry in response.entries:
         if isinstance(entry, dropbox.files.FolderMetadata):
