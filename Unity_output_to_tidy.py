5ffc1f48cd19fec1c33b94bbce89ce41870996fc
Benjamín <dupre.benjamin@gmail.com>
2023-06-27 14:04:48 +0000

    improvements to reading the file

--- Unity_output_to_tidy.py
+++ Unity_output_to_tidy.py
@@ -1,17 +1,18 @@
-
+# Import Relevant Library. 
 import dropbox
 import pandas as pd
 import io
 
 # Set up the Dropbox API client
-ACCES_TOKEN = 'sl.BhElYgYBYOiToDl6ez_umLpLlxPARiwMHN5nqOHBiuUfVxpHB9TAglXrP_bs5K5o4vNRUi8KW15iTHDOiYIVwXxGapITJwvoSW1scsTbtWlKzqP3XztarAe7M7Z2nY2RwIWfjObfgRvP'
+ACCES_TOKEN = 'sl.BhE9Z28Px03DlMr5EmEByZvYkBN58aRUNdg0VACf0SjoVcmYCIP-hoMUGKo9VwVgBD_Yj6hX7Uoy5FTS4pFzGUmbIZGi8rCbBB5PtxnhtONwSrEYmSeuWiG6Dbr9XzsZXb_qFhywBH7U'
 dbx = dropbox.Dropbox(ACCES_TOKEN)
+
 
 # Specify the path to the file you want to access
 
 FOLDER_PATH='/My Mac (glaroam2-185-117.wireless.gla.ac.uk)/Documents/Research MaxPlank/P1_propioception/Data_Wrangling/Matlab Analysis/Data_Wrangling'
 
-
+########################### FUNCIONES
 def get_partc_fold(fld_path):
     """
     load folder within path given
@@ -26,34 +27,63 @@ def get_partc_fold(fld_path):
                 # Display the folder path
                 folder_path = entry.path_display
                 list_folders.append(folder_path)
-                print("Folder path:", folder_path)
+                #print("Folder path:", folder_path)
         return list_folders
                         
     except dropbox.exceptions.HttpError as err:
         print(f"Error listing folder: {err}")
 
-
-"""FUNCIONES"""
-
-
-def read_csv_from_dropbox(path):
+def read_ptcp_sets_from_dropbox(list_sets):
     """
     read csv files
     """
-    path= path + "/everything.csv"
-    _, response = dbx.files_download(path)    
-    csv_data = response.content    
-    # Read the CSV data into a Pandas DataFrame
-     
-    df = pd.read_csv(io.StringIO(csv_data.decode('utf-8')),
-                      header=0, skiprows=133)   # There is some curruption in the files, so we skip first second 
-    # Optionally, return the DataFrame or perform additional processing
-    return df
+    df_sets=[]
+    column_names = ['time', 'Milliseconds', 'levelCounter', 'correctCounter',
+                'leftHandPosition', 'leftHandRotation', 'rightHandPosition',
+                'rightHandRotation', 'redBallPosition', 'redBallRotation',
+                'leftHandGrab', 'rightHandGrab', 'feedbackType',
+                'leftHandVibration', 'rightHandVibration', 'correctBallPosition',
+                'lastTemplateBallPosition', 'areCalibratingGhostHandsActive',
+                'areGrabbingGhostHandsActive', 'calibrationState',
+                'isCalibrationBlocked', 'grabbingState', 'buttonHasBeenPressed',
+                'buttonCurrentlyPressed', 'headPosition', 'headRotation',
+                'isExplosionTriggered', 'ECG']
+    column_types = ['string', 'float64', 'float64', 'float64',
+                'string', 'string', 'string', 'string', 'string',
+                'string', 'boolean', 'boolean', 'category',
+                'boolean', 'boolean', 'string', 'string',
+                'boolean', 'boolean', 'boolean', 'string',
+                'boolean', 'string', 'boolean', 'boolean',
+                'string', 'string', 'object']
+    
+    accumulated_data = []  # Accumulate individual CSV data
+
+    for folder in list_sets:
+        path= folder + "/everything.csv"
+        _, response = dbx.files_download(path)
+
+        csv_data = response.content
+
+        # Read the CSV data into a Pandas DataFrame
+        df = pd.read_csv(io.StringIO(csv_data.decode('utf-8')),
+                      # There is some curruption in the files, so we skip first second
+                      header=0, skiprows=133, delimiter=';', 
+                      names=column_names, dtype=dict(zip(column_names, column_types))).to_dict('records')
 
 
+        accumulated_data.extend(df)  # Accumulate the data
+    
+    # Concatenate all DataFrames in df_sets into one big DataFrame
+    combined_df = pd.concat(df_sets, ignore_index=True)
 
+    # Convert 'ECG' column decimal separators from commas to decimal points
+    combined_df['ECG'] = combined_df['ECG'].str.replace(',', '.').astype('float32')
+    
+    # Return the DataFrame or perform additional processing
+    return combined_df
 
 # Excecution of Functions. 
-list_folders = get_partc_fold(FOLDER_PATH)
-list_subfolders= get_partc_fold(list_folders[1])
-df = read_csv_from_dropbox(list_subfolders[0])
+list_fold = get_partc_fold(FOLDER_PATH)
+list_subfolders= get_partc_fold(list_fold[1])
+combined_dfs = read_ptcp_sets_from_dropbox(list_subfolders)
+
