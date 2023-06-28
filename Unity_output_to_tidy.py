2293d82c36dbe877e64c9513cbf287cf6cfa049a
Benjamín <dupre.benjamin@gmail.com>
2023-06-28 19:38:45 +0200

    add

--- Unity_output_to_tidy.py
+++ Unity_output_to_tidy.py
@@ -4,21 +4,25 @@ import pandas as pd
 import io
 
 # Set up the Dropbox API client
-ACCES_TOKEN = 'sl.BhE9Z28Px03DlMr5EmEByZvYkBN58aRUNdg0VACf0SjoVcmYCIP-hoMUGKo9VwVgBD_Yj6hX7Uoy5FTS4pFzGUmbIZGi8rCbBB5PtxnhtONwSrEYmSeuWiG6Dbr9XzsZXb_qFhywBH7U'
-dbx = dropbox.Dropbox(ACCES_TOKEN)
 
+# Constants
+ACCESS_TOKEN = 'sl.BhJy-0WNKzGdm2lLmgtcs5uvCMp_YP8TGl14fMi4lSBOJXuogOl6vz_lraPVXjy6VPuvL1bBaU6CPBOJgxwfIduZc1bNW9IVov0E4BHGCIuVnsrf2IaVf4DoKiLJnRwlcIeY9j7lfZmI'
+
+dbx = dropbox.Dropbox(ACCESS_TOKEN)
 
 # Specify the path to the file you want to access
 
 FOLDER_PATH='/My Mac (glaroam2-185-117.wireless.gla.ac.uk)/Documents/Research MaxPlank/P1_propioception/Data_Wrangling/Matlab Analysis/Data_Wrangling'
 
 ########################### FUNCIONES
+
+# Geting names and folder paths. 
 def get_partc_fold(fld_path):
     """
-    load folder within path given
-    removes __MACOSX
+    Load folders within the given path and remove __MACOSX.
     """
-    list_folders=[]
+    list_folders = []
+    list_names = []
     try:
         response = dbx.files_list_folder(fld_path)
         # Iterate over the entries in the root directory
@@ -26,12 +30,15 @@ def get_partc_fold(fld_path):
             if isinstance(entry, dropbox.files.FolderMetadata):
                 # Display the folder path
                 folder_path = entry.path_display
+                folder_name = entry.name
                 list_folders.append(folder_path)
+                list_names.append(folder_name)
                 #print("Folder path:", folder_path)
-        return list_folders
-                        
+        return list_folders, list_names
     except dropbox.exceptions.HttpError as err:
         print(f"Error listing folder: {err}")
+
+# Getting data_sets for on participant.
 
 def read_ptcp_sets_from_dropbox(list_sets):
     """
@@ -48,7 +55,7 @@ def read_ptcp_sets_from_dropbox(list_sets):
                 'isCalibrationBlocked', 'grabbingState', 'buttonHasBeenPressed',
                 'buttonCurrentlyPressed', 'headPosition', 'headRotation',
                 'isExplosionTriggered', 'ECG']
-    column_types = ['string', 'float64', 'float64', 'float64',
+    column_types = ['string','integer', 'integer', 'integer',
                 'string', 'string', 'string', 'string', 'string',
                 'string', 'boolean', 'boolean', 'category',
                 'boolean', 'boolean', 'string', 'string',
@@ -58,7 +65,7 @@ def read_ptcp_sets_from_dropbox(list_sets):
     
     accumulated_data = []  # Accumulate individual CSV data
 
-    for folder in list_sets:
+    for i, folder in enumerate(list_sets):
         path= folder + "/everything.csv"
         _, response = dbx.files_download(path)
 
@@ -67,23 +74,31 @@ def read_ptcp_sets_from_dropbox(list_sets):
         # Read the CSV data into a Pandas DataFrame
         df = pd.read_csv(io.StringIO(csv_data.decode('utf-8')),
                       # There is some curruption in the files, so we skip first second
-                      header=0, skiprows=133, delimiter=';', 
+                      skiprows=133, delimiter=';', 
                       names=column_names, dtype=dict(zip(column_names, column_types))).to_dict('records')
-
+        
+        # Add 'trail_set' column with iteration number and 'ptcp' column
+        for record in df:
+            record['trial_set'] = (i + 1)
+            record['ptcp'] = list_names[1]
 
         accumulated_data.extend(df)  # Accumulate the data
     
-    # Create a single DataFrame directly from the accumulated data
-    combined_df = pd.DataFrame(accumulated_data)
+        # Create a single DataFrame directly from the accumulated data
+        combined_df = pd.DataFrame(accumulated_data)
 
-    # Convert 'ECG' column decimal separators from commas to decimal points
-    combined_df['ECG'] = combined_df['ECG'].str.replace(',', '.').astype('float32')
-    
+        # Convert 'ECG' column decimal separators from commas to decimal points
+        combined_df['ECG'] = combined_df['ECG'].str.replace(',', '.').astype('float32')
+        combined_df['time'] = pd.to_datetime(combined_df['time'], format='%Y-%m-%d %H:%M:%S.%f')
     # Return the DataFrame or perform additional processing
     return combined_df
 
-# Excecution of Functions. 
-list_fold = get_partc_fold(FOLDER_PATH)
-list_subfolders= get_partc_fold(list_fold[1])
-combined_dfs = read_ptcp_sets_from_dropbox(list_subfolders)
+
+
+
+
+########################### Excecution of Functions. 
+list_fold,list_names = get_partc_fold(FOLDER_PATH)
+list_ptcp_fold, list_ptcp_names = get_partc_fold(list_fold[1])
+combined_dfs = read_ptcp_sets_from_dropbox(list_ptcp_fold)
 
