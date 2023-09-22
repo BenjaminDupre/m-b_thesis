47ea27b6f199bbdc3f24b279a71ef70b7c45ec06
Benjamín <dupre.benjamin@gmail.com>
2023-09-22 07:53:32 +0000

    Pending changes exported from your codespace

--- unity_output_to_tidy.py
+++ unity_output_to_tidy.py
@@ -117,34 +117,6 @@ def read_ptcp_sets_from_dropbox(path_sets, ptcp_names):
     # Return the DataFrame or perform additional processing
     return combined_df
 
-def get_one_feedback_per_trail(dataf):
-    # Group the data and extract unique feedbackType values per participant, set, and levelCounter
-    unique_feedback_types = dataf.groupby(['ptcp', 'trial_set', 'levelCounter'])['feedbackType'].unique()
-
-    # Remove duplicate feedback types based on the specified conditions
-    for index, feedback_types in unique_feedback_types.items():
-        ptcp, set_val, level_counter = index
-        feedback_types = feedback_types[np.isin(feedback_types, ['incongruent', 'none', 'congruent'])]
-        if level_counter != 0 and feedback_types.size > 1:
-            previous_level_counter = level_counter - 1
-            previous_feedback_types = unique_feedback_types.get((ptcp, set_val, previous_level_counter))
-            if previous_feedback_types is not None :
-                if previous_feedback_types.size==1:
-                    last_element = previous_feedback_types
-                elif previous_feedback_types.size > 1:
-                    last_element = np.asarray(previous_feedback_types)[-1]
-                else:
-                    last_element = None
-            if last_element is not None and feedback_types[0] == last_element :
-                feedback_types = feedback_types[1:]
-        elif level_counter == 0  and feedback_types.size > 1:
-            feedback_types = feedback_types[feedback_types != 'none']
-        unique_feedback_types[index] = feedback_types
-        feedback_df=unique_feedback_types.reset_index()
-        feedback_df= pd.DataFrame(feedback_df) 
-
-    return  feedback_df
-
 def find_ball_position_changes(data):
     B = pd.DataFrame()
     log_file = open("log_button.txt","w")
@@ -215,7 +187,39 @@ def creating_close_trial(data):
     })
     close_df.reset_index(drop=True, inplace=True)
     return close_df 
-import pandas as pd
+
+def get_one_feedback_per_trail(dataf,close_df):
+    # Group the data and extract unique feedbackType values per participant, set, and levelCounter
+    unique_feedback_types = dataf.groupby(['ptcp', 'trial_set', 'levelCounter'])['feedbackType'].unique()
+
+    # Remove duplicate feedback types based on the specified conditions
+    for index, feedback_types in unique_feedback_types.items():
+        ptcp, set_val, level_counter = index
+        feedback_types = feedback_types[np.isin(feedback_types, ['incongruent', 'none', 'congruent'])]
+        if level_counter != 0 and feedback_types.size > 1:
+            previous_level_counter = level_counter - 1
+            previous_feedback_types = unique_feedback_types.get((ptcp, set_val, previous_level_counter))
+            if previous_feedback_types is not None :
+                if previous_feedback_types.size==1:
+                    last_element = previous_feedback_types
+                elif previous_feedback_types.size > 1:
+                    last_element = np.asarray(previous_feedback_types)[-1]
+                else:
+                    last_element = None
+            if last_element is not None and feedback_types[0] == last_element :
+                feedback_types = feedback_types[1:]
+        elif level_counter == 0  and feedback_types.size > 1:
+            feedback_types = feedback_types[feedback_types != 'none']
+        # WARNING: Last resource condition. Not to pro but effective 
+        if  feedback_types.size > 1:
+            row_n = close_df[(close_df['levelCounter'] == level_counter) & (close_df['trial_set'] == set_val)]['row_close']
+            feedback_types = f_ptcp_df[f_ptcp_df['index'] == row_n]['feedbackType']   
+        unique_feedback_types[index] = feedback_types
+    feedback_df=unique_feedback_types.reset_index()
+    feedback_df= pd.DataFrame(feedback_df) 
+
+    return  feedback_df
+
 
 def get_correct(data):
     A = pd.DataFrame(columns=['Set Number', 'Level Counter', 'correctCounter', 'Change Flag'])
@@ -245,21 +249,21 @@ def main():
     g_ptcp_path, g_ptcp_names = get_fold(folder_path[1])
     # 3.Read participant datasets from Dropbox into DF.
     ptcp_df = read_ptcp_sets_from_dropbox(g_ptcp_path,g_ptcp_names)
-    # 4. Procces stimulus type ("Congruent", "Incongruent", "None")
-    feedback_df = get_one_feedback_per_trail(ptcp_df)
     # 5. Find Trials Starts  (when ball changes first position)
     start_df = find_ball_position_changes(ptcp_df)
     # 6. Find Trials Closure (when level counter changes)
     close_df = creating_close_trial(ptcp_df) 
+    # 4. Procces stimulus type ("Congruent", "Incongruent", "None")
+    feedback_df = get_one_feedback_per_trail(ptcp_df,close_df)
     # 6.  Merging Start and Close. 
     merged_df = pd.merge(close_df, start_df, on=['levelCounter', 'trial_set'], how='left')
     # 8.  Merging Feedbacktype 
     # 9. Creating time (seconds) and 
     merged_df["time_secs"] = (merged_df["row_close"] - pd.to_numeric(merged_df["row_start"]))/ 133
-    #9. Merge Feedbacktype
-    #merged_df= pd.merge(merged_df, feedback_df, on=['ptcp','levelCounter', 'trial_set'], how='left')
+    #10. Addin participants label 
     merged_df["ptcp"] = folder_names[1] # needs to be equal to folders path - change when looping
-    
+    #11. Merge Feedbacktype
+    merged_df_bugrun= pd.merge(merged_df, feedback_df, on=['ptcp','levelCounter', 'trial_set'], how='left')
     return g_ptcp_path, g_ptcp_names , ptcp_df, merged_df, feedback_df
 
 ########################### Excecution of Main Function ##
@@ -267,7 +271,8 @@ def main():
 
 if __name__ == '__main__':
     f_ptcp_path, f_ptcp_names, f_ptcp_df, merged_df, feedback_df = main()
-    
+
+merged_df_bugrun.to_csv('clean_merged.csv')    
 ### NEXT TO ADD CORRECT NUMBER AND ADD MORE ITERATIONS: 
 
 
